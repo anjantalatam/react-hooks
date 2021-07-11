e436482fec5caaa16561089946454e633d7fb0bc
anjant-uptycs <anjant@uptycs.com>
2021-07-11 18:26:00 +0530

    13 Fetching data useEffect-2 based on the ID

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,29 +2,30 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function DataFetching() {
-  const [posts, setPosts] = useState([]);
-  const [error, setError] = useState("");
+  const [post, setPost] = useState([]);
+  const [id, setId] = useState(1);
   useEffect(() => {
     axios
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(`https://jsonplaceholder.typicode.com/posts/${id}`)
       .then((res) => {
         console.log(res);
-        setPosts(res.data);
+        setPost(res.data);
       })
       .catch((err) => {
         console.log(err);
-        setError(JSON.stringify(err));
       });
-  }, []);
+  }, [id]);
   return (
     <div>
-      {error || (
-        <ul>
-          {posts.map((post) => (
-            <li key={post.id}>{post.title}</li>
-          ))}
-        </ul>
-      )}
+      <input type='text' value={id} onChange={(e) => setId(e.target.value)} />
+      <div>{post.title}</div>
+      {
+        // <ul>
+        //   {post.map((post) => (
+        //     <li key={post.id}>{post.title}</li>
+        //   ))}
+        // </ul>
+      }
     </div>
   );
 }
